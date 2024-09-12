aa8d9ddf1dfd361175d87fdecfbfb8db79e25d91
Vinicius Gomes <viniciusgomesf33@outlook.com>
2024-09-12 00:15:22 -0300

    style: adjust css view-list

--- src/app/components/acess-list/acess-list.component.ts
+++ src/app/components/acess-list/acess-list.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ListService } from '../services/list.service';
+import { ListService } from '../../services/list.service';
 
 @Component({
   selector: 'app-access-list',
